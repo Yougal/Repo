20be1caadc8c03931c3ba78fe98d00bb3c4511a6
yougal.bisht <yougal.bisht@192.168.38.32>
2016-05-12 15:30:41 +0530

    origin and destination

--- google-distance-api/src/main/java/com/yougal/GoogleDistanceApiApplication.java
+++ google-distance-api/src/main/java/com/yougal/GoogleDistanceApiApplication.java
@@ -5,22 +5,18 @@ import com.google.gson.GsonBuilder;
 import com.google.maps.DistanceMatrixApi;
 import com.google.maps.DistanceMatrixApiRequest;
 import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
 import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.GeocodingResult;
 
 public class GoogleDistanceApiApplication {
 
 	//AIzaSyBzuEW0zymhC_b3ojVd7Rd6g22c0Q93wXU
 	public static void main(String[] args) throws Exception {
-		// Replace the API key below with a valid API key.
-		/**GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyBzuEW0zymhC_b3ojVd7Rd6g22c0Q93wXU");
-		DistanceMatrixApi distanceMatrixApi = DistanceMatrixApi.getDistanceMatrix(context, "Seattle", "San Francisco");
-		
-		GeocodingResult[] results =  GeocodingApi.geocode(context,
-		    "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
-		System.out.println(results[0].formattedAddress);	**/
-		
 		GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyBzuEW0zymhC_b3ojVd7Rd6g22c0Q93wXU");
-		DistanceMatrixApiRequest distanceMatrixApiRequest = DistanceMatrixApi.getDistanceMatrix(context, new String[]{"Seattle"}, new String[]{"San Francisco"});
+		GeocodingResult[] origin  = GeocodingApi.geocode(context,"467 herndon parkway").await();
+		GeocodingResult[] destination  = GeocodingApi.geocode(context,"sunrise valley drive herndon va").await();
+		DistanceMatrixApiRequest distanceMatrixApiRequest = DistanceMatrixApi.getDistanceMatrix(context, new String[]{origin[0].formattedAddress}, new String[]{destination[0].formattedAddress});
 		DistanceMatrix result = distanceMatrixApiRequest.await();
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String jSonResult = gson.toJson(result);
