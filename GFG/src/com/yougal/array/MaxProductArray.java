d63fba181fec652368342c5edcfc6c36712a3c49
Yougal Bisht <yougal.bisht@gmail.com>
2018-07-12 13:07:56 -0400

    correct product for max continous product

--- GFG/src/com/yougal/array/MaxProductArray.java
+++ GFG/src/com/yougal/array/MaxProductArray.java
@@ -4,28 +4,37 @@ public class MaxProductArray {
 	
 	 public static void main (String[] args) {
 		 
-	        int arr[] = {1, -2, -3, 0, 7, -8, -2};
+	        int arr[] = {1, -2, 7,8,-1,-9,891};
 	        System.out.println("Maximum Sub array product is "+
 	                            maxSubarrayProduct(arr));
 	 }
 
 	private static int maxSubarrayProduct(int[] arr) {
-		int maxProduct=arr[0];
-		int productSoFar=arr[0];
-		for (int i = 1; i < arr.length; i++) {
-			
-			productSoFar*=arr[i];
-			if(productSoFar==0){
-				if(i+1<arr.length){
-					productSoFar=arr[i+1];
-					i++;
-				}
+		int maxProduct=Integer.MIN_VALUE;
+		int productSoFar1=1;
+		int productSoFar2=1;
+		int flip =1;
+		int negativeNumber=0;
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i]>0 && flip==1) {
+				productSoFar1*=arr[i];
+				maxProduct=Math.max(maxProduct, productSoFar1);
+			}else if(arr[i]>0 && flip==2) {
+				productSoFar2*=arr[i];
+				maxProduct=Math.max(maxProduct, productSoFar2);
+			}else if(arr[i]<0 && flip==1) {
+				negativeNumber=arr[i];
+				flip=2;
+			}else if(arr[i]<0 && flip==2) {
+				productSoFar1 = productSoFar1*productSoFar2 * negativeNumber * arr[i];
+				flip=1;
+				maxProduct=Math.max(maxProduct, productSoFar1);
+				productSoFar2=1;
+			}else {
+				productSoFar1=1;
+				productSoFar2=1;
+				flip=1;
 			}
-			
-			if(productSoFar>maxProduct){
-				maxProduct=productSoFar;
-			}
-			
 		}
 		return maxProduct;
 	}
